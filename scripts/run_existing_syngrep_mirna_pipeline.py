54e3c36939cefa9606d123db48ca7a9cc0919887
Nikolaos Mitsopoulos <mitsopoulos@watson.bio.ifi.lmu.de>
2026-08-15 14:53:41 +0200

    minor

--- scripts/run_existing_syngrep_mirna_pipeline.py
+++ scripts/run_existing_syngrep_mirna_pipeline.py
@@ -1,7 +1,7 @@
 from pipelines.mirna import run_existing_mirna_pipeline
 from textmining.config import ExistingSyngrepMirnaConfig
 from textmining.paths import OUTPUTS_DIR
-from textmining.resources import CORPUS_SAMPLE
+from textmining.resources import CORPUS_DIR
 
 if __name__ == '__main__':
     mirna_out = OUTPUTS_DIR / 'mirna'
@@ -10,7 +10,6 @@ if __name__ == '__main__':
         hits_path= mirna_out / 'mirna_out.hits',
         synfile_map_path= mirna_out / 'synfile.map',
         synfile_type_map_path= mirna_out / 'synfile_type.map',
-        sentence_path = CORPUS_SAMPLE 
+        sentence_path = CORPUS_DIR 
     )
-    print(f'Running on {config.sentence_path}')
     run_existing_mirna_pipeline(config=config)
--- scripts/run_mirna_pipeline.py
+++ scripts/run_mirna_pipeline.py
@@ -1,6 +1,5 @@
 from pipelines.mirna import run_mirna_pipeline
-from textmining.resources import CORPUS_SAMPLE
+from textmining.resources import CORPUS_DIR
 
 if __name__ == '__main__':
-    print(f'Running on {CORPUS_SAMPLE}')
-    run_mirna_pipeline('mirna_out', sentence_path=CORPUS_SAMPLE)
+    run_mirna_pipeline('mirna_out', sentence_path=CORPUS_DIR)
